fa57078fcd9b5343c9751418a993f4e1ac256e47
vikasraria14 <38049874+vikasraria14@users.noreply.github.com>
2022-09-04 00:12:34 +0530

    working fine again

--- src/Components/Middle/Chats/Chats.js
+++ src/Components/Middle/Chats/Chats.js
@@ -2,12 +2,22 @@ import { useSelector } from 'react-redux'
 //import { ChatData } from './ChatText';
 import User from './User';
 import "./Chats.css"
+import React, { useEffect, useRef } from 'react'
 
 const Chats=()=>{
     const ChatData=useSelector(state=>state.chat)
     console.log("chat ",ChatData)
     const SelectedUser=ChatData.find(chat=>chat.selected)
     const id=SelectedUser.id;
+    const messagesEndRef = useRef(null)
+
+    const scrollToBottom = () => {
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
+      }
+    
+      useEffect(() => {
+        scrollToBottom()
+      }, [SelectedUser]);
     
     if(true)
     {
@@ -20,7 +30,9 @@ const Chats=()=>{
                             return <User key={chat1.message} chat={chat1} id={id}/>
                         }
                     )
+                    
                 }
+                <div ref={messagesEndRef} />
                 
             </div>
         ) 
--- src/Reducers/ChatText.js
+++ src/Reducers/ChatText.js
@@ -3,7 +3,7 @@ export const ChatData = [
       name: 'Henry Boyd',
       firstname:"Henry",
       archived:false,
-      selected:false,
+      selected:true,
       mail:"henryboyd@gmail.com",
       image:'../../Assets/Henry.png',
       id: 1,
@@ -56,7 +56,210 @@ export const ChatData = [
         archived:false,      
         mail:"marthacurtis@gmail.com",
         id: 2,
-        selected:true,
+        selected:false,
+        image:'../../Assets/Martha.png',
+        chat: [
+          {
+            from:"User2",
+            message: 'Hi',
+            date: '2019-05-30T18:39:34.091Z'          
+          },
+          {
+              from:"User1",
+              message: 'Hello',
+              date: '2019-05-30T18:39:34.091Z'          
+            },
+            {
+              from:"User2",
+              message: '"What plans for today?"',
+              date: '2019-05-30T18:39:34.091Z'          
+            },
+            {
+              from:"User1",
+              message: 'Nothing much. How about you?',
+              date: '2019-05-30T18:39:34.091Z'          
+            },
+            {
+              from:"User2",
+              message: 'Planning to go to a movie. Wanna come?',
+              date: '2019-05-30T18:39:34.091Z'          
+            },
+            {
+                from:"User1",
+                message: 'Sure why not.',
+                date: '2019-05-30T18:39:34.091Z'          
+             },
+              {
+                from:"User2",
+                message: 'Great. see you then.',
+                date: '2019-05-30T18:39:34.091Z'          
+              },
+              {
+                from:"User1",
+                message: 'ya bye.',
+                date: '2019-05-30T18:39:34.091Z'          
+              }
+        ]
+      },
+      {      
+        name: 'Phillip Tucker',
+        firstname:'Phillip',
+        archived:false,      
+        mail:"philliptucker@gmail.com",
+        id: 3,
+        selected:false,
+        image:'../../Assets/Martha.png',
+        chat: [
+          {
+            from:"User2",
+            message: 'Hi',
+            date: '2019-05-30T18:39:34.091Z'          
+          },
+          {
+              from:"User1",
+              message: 'Hello',
+              date: '2019-05-30T18:39:34.091Z'          
+            },
+            {
+              from:"User2",
+              message: '"What plans for today?"',
+              date: '2019-05-30T18:39:34.091Z'          
+            },
+            {
+              from:"User1",
+              message: 'Nothing much. How about you?',
+              date: '2019-05-30T18:39:34.091Z'          
+            },
+            {
+              from:"User2",
+              message: 'Planning to go to a movie. Wanna come?',
+              date: '2019-05-30T18:39:34.091Z'          
+            },
+            {
+                from:"User1",
+                message: 'Sure why not.',
+                date: '2019-05-30T18:39:34.091Z'          
+             },
+              {
+                from:"User2",
+                message: 'Great. see you then.',
+                date: '2019-05-30T18:39:34.091Z'          
+              },
+              {
+                from:"User1",
+                message: 'ya bye.',
+                date: '2019-05-30T18:39:34.091Z'          
+              }
+        ]
+      },
+      {      
+        name: 'Christine Reid',
+        firstname:'Martha',
+        archived:false,      
+        mail:"chrestienreid@gmail.com",
+        id: 4,
+        selected:false,
+        image:'../../Assets/Martha.png',
+        chat: [
+          {
+            from:"User2",
+            message: 'Hi',
+            date: '2019-05-30T18:39:34.091Z'          
+          },
+          {
+              from:"User1",
+              message: 'Hello',
+              date: '2019-05-30T18:39:34.091Z'          
+            },
+            {
+              from:"User2",
+              message: '"What plans for today?"',
+              date: '2019-05-30T18:39:34.091Z'          
+            },
+            {
+              from:"User1",
+              message: 'Nothing much. How about you?',
+              date: '2019-05-30T18:39:34.091Z'          
+            },
+            {
+              from:"User2",
+              message: 'Planning to go to a movie. Wanna come?',
+              date: '2019-05-30T18:39:34.091Z'          
+            },
+            {
+                from:"User1",
+                message: 'Sure why not.',
+                date: '2019-05-30T18:39:34.091Z'          
+             },
+              {
+                from:"User2",
+                message: 'Great. see you then.',
+                date: '2019-05-30T18:39:34.091Z'          
+              },
+              {
+                from:"User1",
+                message: 'ya bye.',
+                date: '2019-05-30T18:39:34.091Z'          
+              }
+        ]
+      },
+      {      
+        name: 'Jerry Guzmen',
+        firstname:'Jerry',
+        archived:false,      
+        mail:"jerry@gmail.com",
+        id: 5,
+        selected:false,
+        image:'../../Assets/Martha.png',
+        chat: [
+          {
+            from:"User2",
+            message: 'Hi',
+            date: '2019-05-30T18:39:34.091Z'          
+          },
+          {
+              from:"User1",
+              message: 'Hello',
+              date: '2019-05-30T18:39:34.091Z'          
+            },
+            {
+              from:"User2",
+              message: '"What plans for today?"',
+              date: '2019-05-30T18:39:34.091Z'          
+            },
+            {
+              from:"User1",
+              message: 'Nothing much. How about you?',
+              date: '2019-05-30T18:39:34.091Z'          
+            },
+            {
+              from:"User2",
+              message: 'Planning to go to a movie. Wanna come?',
+              date: '2019-05-30T18:39:34.091Z'          
+            },
+            {
+                from:"User1",
+                message: 'Sure why not.',
+                date: '2019-05-30T18:39:34.091Z'          
+             },
+              {
+                from:"User2",
+                message: 'Great. see you then.',
+                date: '2019-05-30T18:39:34.091Z'          
+              },
+              {
+                from:"User1",
+                message: 'ya bye.',
+                date: '2019-05-30T18:39:34.091Z'          
+              }
+        ]
+      },{      
+        name: 'Russell Williams',
+        firstname:'Martha',
+        archived:false,      
+        mail:"marthacurtis@gmail.com",
+        id: 6,
+        selected:false,
         image:'../../Assets/Martha.png',
         chat: [
           {
